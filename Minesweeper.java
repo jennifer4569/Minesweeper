a2717878a57ea54ac91d334af5022836500f7fd8
Jennifer Zhang <jennifer.newyork@aol.com>
2017-01-04 22:10:35 -0500

    Started on the feature in which each block counts the number of mines around it

--- Minesweeper.java
+++ Minesweeper.java
@@ -19,21 +19,47 @@ public class Minesweeper extends JFrame implements ActionListener{
 
 	gridButton = new JButton[width][length];
 	gridBlock = new Block[width][length];
-	for(int y = 0; y < length; y++){
-	    for(int x = 0; x < width; x++){
+	
+	setupGrid(width,length);
+	findAndSetNumMines(width,length);
+    }
+
+    public void setupGrid(int w, int l){
+	for(int y = 0; y < l; y++){
+	    for(int x = 0; x < w; x++){
 		gridBlock[x][y] = new Block(x, y);
 
 		gridButton[x][y] = new JButton("" + gridBlock[x][y].getBomb());
 		gridButton[x][y].addActionListener(this);
-		gridButton[x][y].setActionCommand("" + x + "," + y);
+		//gridButton[x][y].setActionCommand("" + x + "," + y);
 		
 		pane.add(gridButton[x][y]);
 	    }
 	}
     }
+    
+    public void findAndSetNumMines(int w, int l){
+	int store; //store will contain the number of mines around the block
+	for(int y = 0; y < l; y++){
+	    for(int x = 0; x < w; x++){
+		store = findNumMines(x,y);
+		gridBlock[x][y].setNumMines(store);
+		gridButton[x][y].setActionCommand("" + store);
+	    }
+	}
+    }
+    public int findNumMines(int x, int y){
+	int count = 0;
+	//...BODY HERE!
+	return count;
+    }
+
+
+    
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
 	System.out.println(event);
+	//gridButton[0][0].setText("HI!");
     }
 	
     public static void main(String[] args){
