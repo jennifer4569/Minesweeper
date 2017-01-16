abcd359989f0ee77e26f08620add83a1a7c5c4eb
BermetKalmakova <bermetkalmakova@gmail.com>
2017-01-16 18:06:38 -0500

    added win page

---
+++ Win.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Win extends JFrame implements ActionListener{
+    private Container pane;
+
+    public Win(){
+	this.setTitle("Minesweeper");
+	this.setSize(400,200);
+	this.setLocation(0,0);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	pane = this.getContentPane();
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+	JButton newgame = new JButton("New Game");
+	newgame.addActionListener(this);
+	newgame.setActionCommand("new");
+	newgame.setAlignmentX(Component.CENTER_ALIGNMENT);
+	JLabel name = new JLabel("MINESWEEPER");
+	name.setFont(new Font("FuturaBlack BT", Font.BOLD, 40));
+	name.setAlignmentX(Component.CENTER_ALIGNMENT);
+	JLabel msg = new JLabel("You Won!");
+	msg.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	pane.add(name);
+	pane.add(msg);
+	pane.add(newgame);
+    }
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	Main g = new Main();
+	g.setVisible(true);
+	dispose();
+    }
+    
+    
+}
