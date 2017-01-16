36bed29892f0a3a596cc0655be9517dacab74086
BermetKalmakova <bermetkalmakova@gmail.com>
2017-01-16 01:03:11 -0500

    added custom option

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 
 public class Main extends JFrame implements ActionListener{
     private Container pane;
+    private JTextField dim;
     
     public Main(){
 	this.setTitle("Minesweeper");
@@ -12,21 +13,45 @@ public class Main extends JFrame implements ActionListener{
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	pane = this.getContentPane();
 	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        JButton easy = new JButton("Easy\n8x8 Puzzle");
+        JButton easy = new JButton("<html>Easy<br />8x8 Puzzle</html>");
 	easy.addActionListener(this);
 	easy.setActionCommand("easy");
+	easy.setAlignmentX(Component.CENTER_ALIGNMENT);
 	
-	JButton medium = new JButton("Medium\n16x16 Puzzle");
+	JButton medium = new JButton("<html>Medium<br />16x16 Puzzle</html>");
 	medium.addActionListener(this);
 	medium.setActionCommand("medium");
+	medium.setAlignmentX(Component.CENTER_ALIGNMENT);
 	
-	JButton hard = new JButton("Hard\n20x20 Puzzle");
+	JButton hard = new JButton("<html>Hard<br />20x20 Puzzle</html>");
 	hard.addActionListener(this);
 	hard.setActionCommand("hard");
+	hard.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	JLabel name = new JLabel("MINESWEEPER");
+	name.setFont(new Font("FuturaBlack BT", Font.BOLD, 40));
+	name.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	JLabel custom = new JLabel("Custom size:");
+	custom.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	dim = new JTextField(10);
+	dim.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+	JButton start = new JButton("Start Custom Game");
+	start.addActionListener(this);
+	start.setActionCommand("start");
+	start.setAlignmentX(Component.CENTER_ALIGNMENT);
+
 	
+	pane.add(name);
 	pane.add(easy);
 	pane.add(medium);
 	pane.add(hard);
+	pane.add(custom);
+	pane.add(dim);
+	pane.add(start);
+
     }
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
@@ -45,6 +70,22 @@ public class Main extends JFrame implements ActionListener{
 	    g.setVisible(true);
 	    dispose();
 	}
+	if(event.equals("start")){
+	    String textfield = dim.getText();
+	    int num = Integer.parseInt(textfield);
+	    if(num < 4){
+		dim.setText("4");
+		num = 4;
+	    }
+	    if(num > 20){
+		dim.setText("20");
+		num = 20;
+	    }
+	    Minesweeper g = new Minesweeper(num,num);
+	    g.setVisible(true);
+	    dispose();
+	}
+	    
     }
     public static void main(String[] args){
     }
